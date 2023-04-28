af241e11085461565036f6f7d2e3d358d278792e
Michelle123211 <M.stolova@seznam.cz>
2023-04-28 15:36:26 +0200

    Different camera views (Cinemachine)

--- Brooom/Assets/Experiments/CameraController.cs
+++ Brooom/Assets/Experiments/CameraController.cs
@@ -6,7 +6,15 @@ public class CameraController : MonoBehaviour
 {
     public float mouseSensitivity = 3f;
     [Tooltip("How long in seconds does it take to ease in the sensitivity after start (to prevent quick jump at the beginning).")]
-    public float sensitivityEaseInDuration = 2f;
+    public float sensitivityEaseInDuration = 1f;
+    [Tooltip("The maximum angle (in absolute value) the camera may rotate in the X axis.")]
+    public float maxAngleX = 60;
+    [Tooltip("The maximum angle (in absolute value) the camera may rotate in the Y axis.")]
+    public float maxAngleY = 120;
+
+    [Tooltip("A list of virtual cameras to switch between (in the exact order, index 0 is the default one).")]
+    public List<GameObject> virtualCameras;
+
 
     // Current rotation of the camera
     private float rotationX = 0f;
@@ -16,19 +24,47 @@ public class CameraController : MonoBehaviour
     private float currentT = 0f;
     private float currentSensitivity = 0f;
 
+    // Current camera view
+    private int currentCameraIndex = 0;
+    private GameObject currentCamera;
+
+
     // Start is called before the first frame update
-    void Start()
-    {
-        // lock the cursor to the center of the screen and hide it
+    void Start() {
+        // Lock the cursor to the center of the screen and hide it
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-        transform.localRotation = Quaternion.Euler(rotationX, rotationY, 0f);
+        // Enable only the first virtual camera
+        if (virtualCameras.Count == 0)
+            Debug.LogError("There are no virtual cameras assigned.");
+        for (int i = 0; i < virtualCameras.Count; i++) {
+            if (i == 0) {
+                currentCamera = virtualCameras[i];
+                virtualCameras[i].SetActive(true);
+            } else {
+                virtualCameras[i].SetActive(false);
+            }
+        }
+        // Reset the rotation
+        currentCamera.transform.localRotation = Quaternion.Euler(rotationX, rotationY, 0f);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Gradually increase the sensitivity over the first "" seconds
+        // Handle switching the view
+        if (Input.GetKeyDown(KeyCode.Tab)) {
+            int previousCameraIndex = currentCameraIndex;
+            currentCameraIndex++;
+            if (currentCameraIndex == virtualCameras.Count)
+                currentCameraIndex = 0;
+            currentCamera = virtualCameras[currentCameraIndex];
+            virtualCameras[currentCameraIndex].SetActive(true);
+            virtualCameras[previousCameraIndex].SetActive(false);
+        }
+
+        // Gradually increase the sensitivity over the first "sensitivityEaseInDuration" seconds
         //  - at first the camera is following the mouse very slowly so it prevents quick jump at the beginning
         if (currentSensitivity < mouseSensitivity) {
             currentT = Mathf.Clamp(currentT + Time.deltaTime / sensitivityEaseInDuration, 0, 1);
@@ -40,9 +76,11 @@ public class CameraController : MonoBehaviour
         float mouseY = Input.GetAxis("Mouse Y") * currentSensitivity;
 
         // Limiting the angle of rotation
-        rotationX = Mathf.Clamp(rotationX - mouseY, -89f, 89f);
-        rotationY = Mathf.Clamp(rotationY + mouseX, -179f, 179f);
+        rotationX = Mathf.Clamp(rotationX - mouseY, -maxAngleX, maxAngleX);
+        rotationY = Mathf.Clamp(rotationY + mouseX, -maxAngleY, maxAngleY);
 
-        transform.localRotation = Quaternion.Euler(rotationX, rotationY, 0f);
+        // Rotating the current virtual camera
+        currentCamera.transform.localRotation = Quaternion.Euler(rotationX, rotationY, 0f);
+
     }
 }
