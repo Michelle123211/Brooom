f48b40b6646b4133826b3441a53b4bab26c7af49
Michelle123211 <M.stolova@seznam.cz>
2023-05-02 14:31:18 +0200

    Basic implementation of rebinding + refactorization

--- Brooom/Assets/Experiments/Rebinding/KeyBindingUI.cs
+++ Brooom/Assets/Experiments/Rebinding/KeyBindingUI.cs
@@ -8,11 +8,11 @@ using UnityEngine.InputSystem;
 public class KeyBindingUI : MonoBehaviour {
 
     [Tooltip("Label displaying the readable name of the action.")]
-    [SerializeField] private TextMeshProUGUI label;
+    [SerializeField] private TextMeshProUGUI actionNameText;
     [Tooltip("An object with a button to start rebinding the key.")]
     [SerializeField] private Button rebindingButton;
     [Tooltip("Label on the button starting rebinding.")]
-    [SerializeField] private TextMeshProUGUI keyButtonText;
+    [SerializeField] private TextMeshProUGUI rebindingButtonText;
     [Tooltip("An object displayed when a rebinding is in process.")]
     [SerializeField] private GameObject waitingForInputText;
     [Tooltip("An object with a button to reset the binding.")]
@@ -20,33 +20,66 @@ public class KeyBindingUI : MonoBehaviour {
 
     private PlayerInput playerInput;
 
+    // The assigned input action
     private InputAction action;
-    private InputActionReference actionReference;
 
+    // For storing information about an ongoing rebinding operation
+    private InputActionRebindingExtensions.RebindingOperation rebindingOperation;
+
+    // Updates the UI according to the action
     public void Initialize(InputAction action, string name) {
+        // Update the UI
         this.action = action;
-        label.text = name;
-        keyButtonText.text = action.bindings[0].ToDisplayString();
-	}
+        actionNameText.text = name;
+        UpdateBindingText();
+    }
 
+    // Makes the binding not configurable
     public void MakeReadOnly() {
         rebindingButton.interactable = false;
         resetButton.SetActive(false);
     }
 
     public void StartRebinding() {
+        // Toggle the UI
         rebindingButton.gameObject.SetActive(false);
         waitingForInputText.SetActive(true);
 
         // Deactivate the map
         playerInput.currentActionMap.Disable();
 
-        action.PerformInteractiveRebinding()
-            .WithControlsExcluding("");
+        rebindingOperation = action.PerformInteractiveRebinding()
+            .WithControlsExcluding("") // TODO: Check the correct path for Escape
+            .OnMatchWaitForAnother(0.1f) // short delay
+            .OnComplete(operation => RebindComplete()) // when the rebinding is complete (input was given)
+            .Start(); // to set it all up
     }
 
-	// Start is called before the first frame update
-	void Start()
+    // Updates the UI to display the current binding
+    private void UpdateBindingText() {
+        int bindingIndex = action.GetBindingIndexForControl(action.controls[0]); // controls[0] - first is the currently active control
+        rebindingButtonText.text = InputControlPath.ToHumanReadableString(
+            action.bindings[bindingIndex].effectivePath,
+            InputControlPath.HumanReadableStringOptions.OmitDevice);
+    }
+
+    private void RebindComplete() {
+        // Update the UI
+        UpdateBindingText();
+
+        // Dispose the allocated memory
+        rebindingOperation.Dispose();
+
+        // Toggle the UI
+        waitingForInputText.SetActive(false);
+        rebindingButton.gameObject.SetActive(true);
+
+        // Activate the map
+        playerInput.currentActionMap.Enable();
+    }
+
+    // Start is called before the first frame update
+    void Start()
     {
         playerInput = InputManager.Instance.GetPlayerInput();
     }
--- Brooom/Assets/Experiments/Rebinding/KeyRebindingUI.cs
+++ Brooom/Assets/Experiments/Rebinding/KeyRebindingUI.cs
@@ -5,10 +5,10 @@ using UnityEngine.UI;
 using UnityEngine.InputSystem;
 
 public class KeyRebindingUI : MonoBehaviour {
-    [Tooltip("Names of the input actions whose binding should not be configured.")]
-    public List<string> inputActionsToExclude = new List<string>();
+    [Tooltip("Input actions whose binding should not be displayed at all.")]
+    public List<InputActionReference> inputActionsToExclude = new List<InputActionReference>();
     [Tooltip("Names of the input actions which should be displayed but not allowed to be modified.")]
-    public List<string> inputActionsReadOnly = new List<string>();
+    public List<InputActionReference> inputActionsReadOnly = new List<InputActionReference>();
     [Tooltip("Names which should be visible to the player in the UI for the specific action (if different from the action name).")]
     public List<InputActionName> actionReadableNames;
 
@@ -18,7 +18,9 @@ public class KeyRebindingUI : MonoBehaviour {
     [SerializeField] private Transform keyBindingParent;
 
 
-    private List<InputAction> inputActions;
+    public void ToggleVisibility() {
+        gameObject.SetActive(!gameObject.activeInHierarchy);
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -34,24 +36,38 @@ public class KeyRebindingUI : MonoBehaviour {
 
 	private void OnEnable() {
         // Refresh the list of bindings
-        inputActions = InputManager.Instance.inputActions;
-        foreach (InputAction action in inputActions) {
-            if (inputActionsToExclude.Contains(action.name)) {
-                continue;
-            } else {
-                KeyBindingUI keyBindingInstance = Instantiate<KeyBindingUI>(keyBinding, keyBindingParent);
-                string name = action.name;
-                // Override redable names if provided
-                foreach (InputActionName actionName in actionReadableNames) {
-                    if (actionName.actionName == action.name) {
-                        name = actionName.readableName;
-                        break;
-                    }
+        // First remove all
+        for (int i = keyBindingParent.childCount - 1; i >= 0; i--) {
+            Destroy(keyBindingParent.GetChild(i).gameObject);
+        }
+        // Then add the current ones
+        foreach (InputAction action in InputManager.Instance.inputActions) {
+            // Exclude the action if necessary
+            bool exclude = false;
+            foreach (InputActionReference actionToExclude in inputActionsToExclude) {
+                if (actionToExclude.action.name == action.name) {
+                    exclude = true;
+                    break;
                 }
-                keyBindingInstance.Initialize(action, name);
-                if (inputActionsReadOnly.Contains(action.name))
+            }
+            if (exclude) continue;
+            // Add binding UI for the action
+            KeyBindingUI keyBindingInstance = Instantiate<KeyBindingUI>(keyBinding, keyBindingParent);
+            string name = action.name;
+            // Override redable names if provided
+            foreach (InputActionName actionName in actionReadableNames) {
+                if (actionName.actionToRename.action.name == action.name) {
+                    name = actionName.readableName;
+                    break;
+                }
+            }
+            keyBindingInstance.Initialize(action, name);
+            // Make read-only if necessary
+            foreach (InputActionReference actionReadOnly in inputActionsReadOnly) {
+                if (actionReadOnly.name == action.name) {
                     keyBindingInstance.MakeReadOnly();
-            
+                    break;
+                }
             }
         }
 	}
@@ -59,8 +75,8 @@ public class KeyRebindingUI : MonoBehaviour {
 
 [System.Serializable]
 public class InputActionName {
-    [Tooltip("Name of the action in the InputActions asset assigned to the PlayerInput component.")]
-    public string actionName;
+    [Tooltip("Action in the InputActions asset assigned to the PlayerInput component whose name should be displayed differently.")]
+    public InputActionReference actionToRename;
     [Tooltip("Name of the action visible to the player in the rebinding UI.")]
     public string readableName;
 }
