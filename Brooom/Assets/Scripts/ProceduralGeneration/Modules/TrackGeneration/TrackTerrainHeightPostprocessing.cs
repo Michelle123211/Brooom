8a0e8df6af11b02284c770e5c0530d5a3a4b2dce
Michelle123211 <M.stolova@seznam.cz>
2023-06-12 16:15:38 +0200

    Minimum offset of hoops above ground
    + Height limited by maximum altitude of the broom

--- Brooom/Assets/Scripts/ProceduralGeneration/Modules/TrackGeneration/TrackTerrainHeightPostprocessing.cs
+++ Brooom/Assets/Scripts/ProceduralGeneration/Modules/TrackGeneration/TrackTerrainHeightPostprocessing.cs
@@ -4,34 +4,79 @@ using UnityEngine;
 
 public class TrackTerrainHeightPostprocessing : LevelGeneratorModule {
 
-	[Tooltip("Each hoop's Y coordinate is adjusted according to the maximum terran height in a close neighbourhood of the given radius.")]
+	[Tooltip("Each hoop's Y coordinate is adjusted according to the maximum terrain height in a close neighbourhood of the given radius.")]
 	public int hoopHeightAreaRadius = 10;
+	[Tooltip("Hoops and checkpoints should be placed in this minimum height above ground..")]
+	public float defaultMinimumOffsetAboveGround = 1;
+	[Tooltip("Some regions may prefer different minimum height above ground than the default one (e.g. larger for forest).")]
+	public List<TrackInRegionHeight> minHeightOffsetOverrides;
 
-	// TODO: Limit the Y coordinate according to the maximum altitude of the broom
-	// TODO: Adjust the terrain height if the track point is beneath it (affect only some neighbourhood, e.g. sphere) - may not be necessary
+
+	private Dictionary<MapRegionType, float> minimumOffsetAboveGround;
+
 	public override void Generate(LevelRepresentation level) {
-		AdjustPointsHeightAboveTerrain(level);
-	}
+		PrepareRegionHeightDictionary(level);
 
-	private void AdjustPointsHeightAboveTerrain(LevelRepresentation level) {
-		// Set Y coordinate of the points above terrain
+		// Change Y coordinate of each track point according to the terrain height in a close neighbourhood, ensure minimum height above ground
 		foreach (var trackPoint in level.track) {
-			// Get maximum height on some small neighbourhood
-			float maxHeight = trackPoint.position.y;
-			int x, y;
-			for (int i = -hoopHeightAreaRadius; i <= hoopHeightAreaRadius; i++) {
-				x = trackPoint.gridCoords.x + i;
-				if (x < 0 || x >= level.pointCount.x) continue;
-				for (int j = -hoopHeightAreaRadius; j <= hoopHeightAreaRadius; j++) {
-					y = trackPoint.gridCoords.y + j;
-					if (y < 0 || y >= level.pointCount.y) continue;
-					if (level.terrain[x, y].position.y > maxHeight)
-						maxHeight = level.terrain[x, y].position.y;
-				}
-			}
-			// Use this height +1 for the hoop
-			trackPoint.position.y = maxHeight + 1;
-			// TODO: Distribute any change to 2 or 3 adjacent points in both sides as well to smooth it out if hoops are too close to each other
+			float height = FindMaximumHeightInNeighbourhood(level, trackPoint);
+			if (trackPoint.position.y < height)
+				trackPoint.position.y = height;
+			// Limit the Y coordinate according to the maximum altitude of the broom
+			trackPoint.position.y = Mathf.Clamp(trackPoint.position.y, 0, level.maxAltitude);
+			// TODO: Distribute any change to 2 or 3 adjacent points in both sides as well to smooth it out - if hoops are too close to each other
 		}
 	}
+
+	private void PrepareRegionHeightDictionary(LevelRepresentation level) {
+		// Prepare Dictionary of minimum height for each region type
+		minimumOffsetAboveGround = new Dictionary<MapRegionType, float>();
+		foreach (var region in level.regions) {
+			minimumOffsetAboveGround.Add(region.Key, defaultMinimumOffsetAboveGround);
+		}
+		// Override minimum height for specific regions
+		if (minHeightOffsetOverrides != null) {
+			foreach (var heightOverride in minHeightOffsetOverrides) {
+				minimumOffsetAboveGround[heightOverride.region] = heightOverride.minimumHeightAboveGround;
+			}
+		}
+	}
+
+	private float FindMaximumHeightInNeighbourhood(LevelRepresentation level, TrackPoint trackPoint) {
+		// Get maximum height in some small neighbourhood
+		float maxHeight = float.MinValue;
+		float height;
+		int x, y;
+		for (int i = -hoopHeightAreaRadius; i <= hoopHeightAreaRadius; i++) {
+			x = trackPoint.gridCoords.x + i;
+			if (x < 0 || x >= level.pointCount.x) continue; // out of bounds check
+			for (int j = -hoopHeightAreaRadius; j <= hoopHeightAreaRadius; j++) {
+				y = trackPoint.gridCoords.y + j;
+				if (y < 0 || y >= level.pointCount.y) continue; // out of bounds check
+				// Get terrain height + minimum offset above ground
+				height = GetMinimumHeightAboveGround(level.terrain[x, y]);
+				if (height > maxHeight)
+					maxHeight = height;
+			}
+		}
+		return maxHeight;
+	}
+
+	// Returns the minimum height a track point can be at (considering the terrain heigth and the minimum offset above ground)
+	private float GetMinimumHeightAboveGround(TerrainPoint terrainPoint) {
+		// Get the terrain height
+		float minHeight = terrainPoint.position.y;
+		// Add minimum offset above ground
+		if (minimumOffsetAboveGround.TryGetValue(terrainPoint.region, out float minHeightOffset))
+			minHeight += minHeightOffset;
+		else
+			minHeight += defaultMinimumOffsetAboveGround;
+		return minHeight;
+	}
+}
+
+[System.Serializable]
+public class TrackInRegionHeight {
+	public MapRegionType region = MapRegionType.NONE;
+	public float minimumHeightAboveGround = 1;
 }
