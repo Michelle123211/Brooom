662a5cfae054112128983e9045c9db15d479c1ca
Michelle123211 <M.stolova@seznam.cz>
2023-06-14 10:50:56 +0200

    Basic infrastructure for bonus

--- Brooom/Assets/Scripts/Gameplay/Bonus.cs
+++ Brooom/Assets/Scripts/Gameplay/Bonus.cs
@@ -6,23 +6,69 @@ using UnityEngine.Events;
 // Common component of all the bonus objects (handles common functionality and triggering the specific effect)
 [RequireComponent(typeof(Collider))]
 public class Bonus : MonoBehaviour {
+    [Tooltip("Material assigned to the bonus.")]
+    public Material bonusMaterial;
+
+    [Tooltip("After how many seconds picked-up bonus is activated again.")]
+    public float reactivationTime = 8;
+
     [Tooltip("What should happen when the bonus is picked up other than the primary effect handled by BonusEffect component (e.g. particles, destroying self).")]
     public UnityEvent pickUpEvent;
 
+    [Tooltip("What should happen when the bonus is activated (e.g. particles).")]
+    public UnityEvent activationEvent;
+    [Tooltip("What should happen when the bonus is deactivated (e.g. particles).")]
+    public UnityEvent deactivationEvent;
+
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Player")) {
-            PlayerController player = other.GetComponent<PlayerController>();
+            // Get the PlayerController component
+            PlayerController player = other.GetComponent<PlayerController>(); // try the colliding object
+            if (player == null)
+                player = other.transform.parent.GetComponent<PlayerController>(); // otherwise try the parent object
+            if (player == null)
+                Debug.LogWarning("OnTriggerEnter() in bonus object is invoked but no PlayerController was found.");
             // Invoke the specific event
             BonusEffect effect = GetComponent<BonusEffect>();
-            if (effect != null)
+            if (effect != null && player != null)
                 effect.ApplyBonusEffect(player);
             // Invoke the common events (e.g. particles, destroying self)
             if (!Utils.IsNullEvent(pickUpEvent))
                 pickUpEvent.Invoke();
+            // Deactivate and start reactivation countdown
+            Invoke(nameof(Activate), reactivationTime);
+            Deactivate();
+            Debug.Log(other.name);
         }
 	}
+    
+    [ContextMenu("Refresh Visual")]
+    public void RefreshVisual() {
+        GetComponentInChildren<MeshRenderer>().material = bonusMaterial;
+    }
+
+    public void Deactivate() {
+        Debug.Log("Deactivating bonus.");
+        if (!Utils.IsNullEvent(deactivationEvent))
+            deactivationEvent.Invoke();
+        // TODO: Wait until the event is done, in the meantime e.g. disable only the collider
+        // TODO: Add some tweening
+        gameObject.SetActive(false);
+    }
+
+    public void Activate() {
+        Debug.Log("Activating bonus.");
+        // TODO: Add some tweening
+        gameObject.SetActive(true);
+        if (!Utils.IsNullEvent(activationEvent))
+            activationEvent.Invoke();
+    }
 
     public void DestroySelf() {
         Destroy(gameObject);
     }
+
+	private void Start() {
+        RefreshVisual();
+	}
 }
