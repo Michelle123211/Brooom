a0b684546d6b9fbed4af3b12c53e8e7bcac0bc0b
Michelle123211 <M.stolova@seznam.cz>
2023-06-23 01:09:55 +0200

    Placing more bonuses in a row

--- Brooom/Assets/Scripts/ProceduralGeneration/Modules/TrackGeneration/BonusPlacement.cs
+++ Brooom/Assets/Scripts/ProceduralGeneration/Modules/TrackGeneration/BonusPlacement.cs
@@ -7,6 +7,9 @@ public class BonusPlacement : LevelGeneratorModule {
     [Tooltip("Placement parameters for all the specific types of bonuses. The top ones have higher priority when occupying a spot.")]
     public List<BonusPlacementParameters> bonuses;
 
+    [Tooltip("The spacing between bonuses in a single row.")]
+    public float spacing = 1.5f;
+
     [Tooltip("An object which will be parent of all the bonus objects in the hierarchy.")]
     public Transform bonusParent;
 
@@ -26,15 +29,26 @@ public class BonusPlacement : LevelGeneratorModule {
         foreach (var bonus in bonuses) {
             for (int i = 0; i < level.bonuses.Count; i++) {
                 if (level.bonuses[i].isEmpty && bonus.pattern[i % bonus.pattern.Count]) {  // populate empty spots according to the pattern
-                    CreateBonusInstance(level.bonuses[i], bonus);
+                    CreateBonusInstances(level, level.bonuses[i], bonus);
                 }
             }
         }
     }
 
-    private void CreateBonusInstance(BonusSpot spot, BonusPlacementParameters bonus) {
-        // Create an instance of the bonus
-        Bonus bonusInstance = Instantiate<Bonus>(bonus.bonusPrefab, spot.position, Quaternion.identity, bonusParent);
+    private void CreateBonusInstances(LevelRepresentation level, BonusSpot spot, BonusPlacementParameters bonus) {
+        // Choose number of bonuses in a single row
+        int min = Mathf.Max(Mathf.Min(bonus.countRange.x, bonus.countRange.y), 0); // at least 0 and make it swap-resistant
+        int max = Mathf.Max(Mathf.Max(bonus.countRange.x, bonus.countRange.y), 0);
+        int count = Random.Range(min, max);
+        if (count == 0) return;
+        // Compute position parameters
+        Vector3 forwardProjected = (level.track[spot.previousHoopIndex + 1].position - level.track[spot.previousHoopIndex].position).WithY(0); // forward vector projected to the XZ plane
+        Vector3 direction = Vector3.Cross(forwardProjected, Vector3.up).normalized; // right vector (in the direction of a row of bonuses)
+        Vector3 leftPosition = spot.position - direction * spacing * ((count - 1) / 2f);
+        // Create instances of the bonus
+        for (int i = 0; i < count; i++) {
+            Instantiate<Bonus>(bonus.bonusPrefab, leftPosition + direction * spacing * i, Quaternion.identity, bonusParent);
+        }
         spot.isEmpty = false;
     }
 
@@ -53,7 +67,7 @@ public class BonusPlacementParameters {
     public string visibleName;
     [Tooltip("Prefab of the bonus object.")]
     public Bonus bonusPrefab;
-    [Tooltip("How many instances should be next to each other in one spot (min and max).")]
+    [Tooltip("How many instances should be next to each other in one spot (min and max, must be non-negative).")]
     public Vector2Int countRange;
     [Tooltip("Pattern describing frequency of occurences (e.g. [false, true] means every other, e.g. [true, false, true] means every first and third).")]
     public List<bool> pattern;
