c44100e1e4e43c33e5e47b851536527ef7b05e25
Michelle123211 <M.stolova@seznam.cz>
2023-09-18 01:52:21 +0200

    Placing opponents on start

--- Brooom/Assets/Scripts/ProceduralGeneration/Modules/Opponents/OpponentsGeneration.cs
+++ Brooom/Assets/Scripts/ProceduralGeneration/Modules/Opponents/OpponentsGeneration.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class OpponentsGeneration : LevelGeneratorModule {
 
-	[Tooltip("Prefab of the opponent.")]
-	public OpponentRandomization opponentPrefab;
 	[Tooltip("Number of opponents to generate.")]
 	public int opponentsCount = 5;
+	[Tooltip("Space between the racers on the start line.")]
+	public float spacing = 2;
+	[Tooltip("Prefab of the opponent.")]
+	public OpponentRandomization opponentPrefab;
 	[Tooltip("An object which will be parent of all opponent objects in the hierarchy.")]
 	public Transform opponentsParent;
 
 	public override void Generate(LevelRepresentation level) {
 		for (int i = 0; i < opponentsCount; i++) {
-			// TODO: Instantiate under the common parent
-			// TODO: Place on the start line (to a position given by the index)
-			// TODO: Randomize the opponent (appearance, name, broom upgrades, equipped spells, minimap icon color)
-			// opponent.Randomize();
+			// Compute the position on the start line (given by the index) - if the opponent count is odd, there is one more to the left of the player
+			int offset = i - ((opponentsCount + 1) / 2);
+			if (offset >= 0) offset += 1;
+			Vector3 position = level.playerStartPosition + offset * Vector3.right * spacing;
+			// Instantiate under the common parent
+			OpponentRandomization opponent = Instantiate<OpponentRandomization>(opponentPrefab, position, Quaternion.identity, opponentsParent);
+			opponent.transform.localScale = 0.5f * opponent.transform.localScale;
+			// Randomize the opponent (appearance, name, broom upgrades, equipped spells, minimap icon color)
+			opponent.Randomize();
 		}
 	}
 }
