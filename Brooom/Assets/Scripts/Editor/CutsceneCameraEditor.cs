f993d2c43a6ab598f875aa4443e332c4a73cdf1a
Michelle123211 <M.stolova@seznam.cz>
2023-09-25 19:20:12 +0200

    CutsceneCamera - editor extension

---
+++ Brooom/Assets/Scripts/Editor/CutsceneCameraEditor.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(CutsceneCamera))]
+public class CutsceneCameraEditor : Editor {
+
+	public override void OnInspectorGUI() {
+		// Display the behaviour type dropdown
+		SerializedProperty behaviourTypeProperty = serializedObject.FindProperty("behaviourType");
+		EditorGUILayout.PropertyField(behaviourTypeProperty);
+
+		// Check the type of cutscene camera behaviour
+		int enumIndex = behaviourTypeProperty.enumValueIndex;
+		CutsceneCameraBehaviour behaviourType = (CutsceneCameraBehaviour)enumIndex;
+
+		// Display corresponding fields
+		DisplayProperties(behaviourType);
+
+		// Save all changes made to the Inspector
+		serializedObject.ApplyModifiedProperties();
+	}
+
+	private void DisplayProperties(CutsceneCameraBehaviour behaviourType) {
+		// Target object
+		if (behaviourType == CutsceneCameraBehaviour.StaticRelativeToObject ||
+			behaviourType == CutsceneCameraBehaviour.MovingRelativeToObject ||
+			behaviourType == CutsceneCameraBehaviour.FollowingObject)
+			EditorGUILayout.PropertyField(serializedObject.FindProperty("target"));
+		// Position
+		EditorGUILayout.PropertyField(serializedObject.FindProperty("position"));
+		// Rotation
+		if (behaviourType != CutsceneCameraBehaviour.FollowingObject)
+			EditorGUILayout.PropertyField(serializedObject.FindProperty("rotation"));
+		// Movement direction and speed
+		if (behaviourType == CutsceneCameraBehaviour.Moving ||
+			behaviourType == CutsceneCameraBehaviour.MovingRelativeToObject) {
+			EditorGUILayout.PropertyField(serializedObject.FindProperty("direction"));
+			EditorGUILayout.PropertyField(serializedObject.FindProperty("speed"));
+		}
+	}
+
+}
