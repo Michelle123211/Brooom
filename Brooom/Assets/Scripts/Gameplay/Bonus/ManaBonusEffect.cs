cb9832d35d88465989f1f9056ab2579cffb03b1d
Michelle123211 <M.stolova@seznam.cz>
2023-10-01 01:20:01 +0200

    Mana/recharge bonus - only when spell is purchased

--- Brooom/Assets/Scripts/Gameplay/Bonus/ManaBonusEffect.cs
+++ Brooom/Assets/Scripts/Gameplay/Bonus/ManaBonusEffect.cs
@@ -14,10 +14,10 @@ public class ManaBonusEffect : BonusEffect {
 	}
 
 	public override bool IsAvailable() {
-		// Available if at least one spell is equipped
-		// TODO: Maybe change later to at least one spell purchased (so the opponents can use spells even when the player does not have any equipped)
-		foreach (var spell in PlayerState.Instance.equippedSpells) {
-			if (spell != null) return true;
+		// Available if at least one spell is purchased (so the opponents can use spells even when the player does not have any equipped but has the option to do so)
+		foreach (var spell in PlayerState.Instance.spellAvailability) {
+			if (spell.Value) // spell is available
+				return true;
 		}
 		return false;
 	}
