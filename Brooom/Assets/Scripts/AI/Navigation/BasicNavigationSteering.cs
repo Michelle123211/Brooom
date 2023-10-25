a36b775ad0ea0f6a48dca492c42d20610d60dfc2
Michelle123211 <M.stolova@seznam.cz>
2023-10-25 04:30:10 +0200

    Basic steering improvement

--- Brooom/Assets/Scripts/AI/Navigation/BasicNavigationSteering.cs
+++ Brooom/Assets/Scripts/AI/Navigation/BasicNavigationSteering.cs
@@ -4,67 +4,60 @@ using UnityEngine;
 
 public class BasicNavigationSteering : NavigationSteering {
 
-	[Tooltip("Interval in seconds between movement direction reevaluations.")]
-	public float refreshInterval = 0.01f;
-
 	[Header("Angle thresholds")]
 	[Tooltip("The current yaw direction is changed only if it is at least this angle from the direction to the target.")]
 	public float yawAngleThreshold = 15f;
 	[Tooltip("The current pitch direction is changed only if it is at least this angle from the direction to the target.")]
 	public float pitchAngleThreshold = 5f;
 
-	[Tooltip("If the angle between current direction and the direction to the target is below this angle, it is considered safe to continue forward in full speed.")]
-	public float goodAngleThreshold = 45f;
 	[Tooltip("If the angle between current direction and the direction to the target is greater than this angle, it is considered better to brake.")]
-	public float badAngleThreshold = 90f;
+	public float brakeAngleThreshold = 90f;
 
-	private float elapsedTime = 0;
-	private ForwardMotion lastForward = ForwardMotion.None;
+	[Header("Input curves")]
+	[Tooltip("Yaw input value depending on the angle.")]
+	public AnimationCurve yawInputCurve;
+	[Tooltip("Pitch input value depending on the angle.")]
+	public AnimationCurve pitchInputCurve;
+	[Tooltip("Forward input value depending on the angle.")]
+	public AnimationCurve forwardInputCurveAngle;
+	[Tooltip("Forward input value depending on distance to the target.")]
+	public AnimationCurve forwardInputCurveDistance;
 
 	public override CharacterMovementValues GetCurrentMovementValue() {
-
-		if (elapsedTime > refreshInterval) {
-			elapsedTime -= refreshInterval;
-			CharacterMovementValues movement = RecomputeMovementValue();
-			lastForward = movement.forwardMotion;
-			return movement;
-		} else {
-			return new CharacterMovementValues(lastForward, YawMotion.None, PitchMotion.None); // don't lower the forward speed
-		}
+		return RecomputeMovementValue();
 	}
 
 	private CharacterMovementValues RecomputeMovementValue() {
 		Vector3 startPosition = this.agent.transform.position;
 		Vector3 targetDirection = targetPosition - startPosition;
+		float distance = Vector3.Distance(startPosition, targetPosition);
 
 		CharacterMovementValues movement = new CharacterMovementValues();
 
 		// Yaw
 		float yawAngle = Vector3.SignedAngle(targetDirection, this.agent.transform.forward, Vector3.up);
+		float yawAngleAbs = Mathf.Abs(yawAngle);
 		if (yawAngle < -yawAngleThreshold) movement.yawMotion = YawMotion.Right;
 		else if (yawAngle > yawAngleThreshold) movement.yawMotion = YawMotion.Left;
 		else movement.yawMotion = YawMotion.None;
+		movement.yawValue = yawInputCurve.Evaluate(yawAngleAbs);
 
 		// Pitch
 		float pitchAngle = Vector3.SignedAngle(targetDirection, this.agent.transform.forward, this.agent.transform.right);
+		float pitchAngleAbs = Mathf.Abs(pitchAngle);
 		if (pitchAngle < -pitchAngleThreshold) movement.pitchMotion = PitchMotion.Down;
 		else if (pitchAngle > pitchAngleThreshold) movement.pitchMotion = PitchMotion.Up;
 		else movement.pitchMotion = PitchMotion.None;
+		movement.pitchValue = pitchInputCurve.Evaluate(pitchAngleAbs);
 
 		// Forward
-		float absYawAngle = Mathf.Abs(yawAngle);
-		if (absYawAngle < goodAngleThreshold) movement.forwardMotion = ForwardMotion.Forward; // if the current direction is reasonable, go forward
-		else if (absYawAngle > badAngleThreshold) movement.forwardMotion = ForwardMotion.Brake; // if it is entirely wrong, brake
-		else movement.forwardMotion = ForwardMotion.None; // otherwise do nothing
-
-
-		Debug.Log($"Current movement: ({movement.forwardMotion}, {movement.yawMotion}, {movement.pitchMotion})");
+		if (yawAngleAbs > brakeAngleThreshold) movement.forwardMotion = ForwardMotion.Brake; // if the current direction is entirely wrong, brake
+		else {
+			movement.forwardMotion = ForwardMotion.Forward;
+			movement.forwardValue = Mathf.Max(forwardInputCurveAngle.Evaluate(yawAngleAbs), forwardInputCurveDistance.Evaluate(distance));
+		}
 
 		return movement;
-	}
-
-	private void Update() {
-		elapsedTime += Time.deltaTime;
 	}
 
 }
