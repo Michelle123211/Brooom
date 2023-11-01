a169562b15cc5d0c71e397816aee77f419253e97
Michelle123211 <M.stolova@seznam.cz>
2023-11-01 13:47:37 +0100

    Camera movement not dependent on time scale

--- Brooom/Assets/Experiments/OpponentAI/ExperimentCameraMouseController.cs
+++ Brooom/Assets/Experiments/OpponentAI/ExperimentCameraMouseController.cs
@@ -15,7 +15,7 @@ public class ExperimentCameraMouseController : MonoBehaviour
     void Update() {
         // Move the camera forward
         if (Input.GetMouseButton(0)) {
-            transform.position += transform.forward * speed * Time.deltaTime;
+            transform.position += transform.forward * speed * Time.unscaledDeltaTime;
         }
         // Rotate the camera based on the mouse movement
         float mouseX = Input.GetAxis("Mouse X");
