a46c17994011fca934be8af99ef29aeb226b13ad
Michelle123211 <M.stolova@seznam.cz>
2023-11-12 02:45:57 +0100

    AI - skill level preparation

--- Brooom/Assets/Scripts/AI/AISkillLevel.cs
+++ Brooom/Assets/Scripts/AI/AISkillLevel.cs
@@ -3,7 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-// Contains all parameters and computations related to errors made by AI agents
-public class AISkillLevel : ScriptableObject {
+// Contains all parameters and computations related to mistakes made by AI agents
+public class AISkillLevel : MonoBehaviour {
+
+	public enum RelationToPlayer {
+		Better,
+		SlightlyBetter,
+		RoughlyTheSame,
+		SlightlyWorse,
+		Worse
+	}
+
+	// Initial values which determine the agent's skill level
+	private PlayerStats baseStatsValues;
+	// Currently used values (derived from the initial values based on distance to the player - rubber banding)
+	private PlayerStats currentStatsValues;
+
+
+	public void Initialize(RelationToPlayer skillLevelRelativeToPlayer) {
+		baseStatsValues = PlayerState.Instance.CurrentStats;
+		// TODO: Modify the baseStatsValues based on the skillLevelRelativeToPlayer
+		currentStatsValues = baseStatsValues;
+	}
+
+	// Returns probability that the agent makes a mistake relevant for the Speed stat (e.g. slow down, skip speed bonus)
+	public float GetSpeedMistakeProbability() {
+		// TODO: Implement
+		return 0;
+	}
+
+	// Returns probability that the agent makes a mistake relevant for the Dexterity stat (e.g. not change direction, collide with obstacles, miss bonus/hoop)
+	public float GetDexterityMistakeProbability() {
+		// TODO: Implement
+		return 0;
+	}
+
+	// Returns probability that the agent makes a mistake relevant for the Precision stat (e.g. skip bonus/hoop, collide with obstacles)
+	public float GetPrecisionMistakeProbability() {
+		// TODO: Implement
+		return 0;
+	}
+
+	// Returns probability that the agent makes a mistake relevant for the Magic stat (e.g. skip mana bonus, prolonged dealy before cast, not cast diverse spells)
+	public float GetMagicMistakeProbability() {
+		// TODO: Implement
+		return 0;
+	}
+
+	private void Update() {
+		// TODO: Compute currentStatsValues from rubber banding
+		float distance = Vector3.Distance(transform.position.WithY(0), RaceController.Instance.playerRacer.characterController.transform.position.WithY(0));
+	}
 
 }
--- Brooom/Assets/Scripts/ProceduralGeneration/Modules/Opponents/OpponentsGeneration.cs
+++ Brooom/Assets/Scripts/ProceduralGeneration/Modules/Opponents/OpponentsGeneration.cs
@@ -6,6 +6,8 @@ public class OpponentsGeneration : LevelGeneratorModule {
 
 	[Tooltip("Number of opponents to generate.")]
 	public int opponentsCount = 5;
+	[Tooltip("Skill levels of the opponents.")]
+	public List<AISkillLevel.RelationToPlayer> skillLevels;
 	[Tooltip("Space between the racers on the start line.")]
 	public float spacing = 2;
 	[Tooltip("Prefab of the opponent.")]
@@ -16,6 +18,10 @@ public class OpponentsGeneration : LevelGeneratorModule {
 	public Color[] opponentColors;
 
 	public override void Generate(LevelRepresentation level) {
+		// Prepare list of skill levels to choose from
+		List<AISkillLevel.RelationToPlayer> remainingSkillLevels = new List<AISkillLevel.RelationToPlayer>();
+		// Instantiate and place the opponents
+		foreach (var skillLevel in skillLevels) remainingSkillLevels.Add(skillLevel);
 		for (int i = 0; i < opponentsCount; i++) {
 			// Compute the position on the start line (given by the index) - if the opponent count is odd, there is one more to the left of the player
 			int offset = i - ((opponentsCount + 1) / 2);
@@ -23,6 +29,12 @@ public class OpponentsGeneration : LevelGeneratorModule {
 			Vector3 position = level.playerStartPosition + offset * Vector3.right * spacing;
 			// Instantiate under the common parent
 			OpponentRandomization opponent = Instantiate<OpponentRandomization>(opponentPrefab, position, Quaternion.identity, opponentsParent);
+			// Determine opponent's skill level
+			if (remainingSkillLevels.Count > 0) {
+				int skillLevelIndex = Random.Range(0, remainingSkillLevels.Count);
+				opponent.GetComponentInChildren<AISkillLevel>().Initialize(remainingSkillLevels[skillLevelIndex]);
+				remainingSkillLevels.RemoveAt(skillLevelIndex);
+			} else opponent.GetComponentInChildren<AISkillLevel>().Initialize(AISkillLevel.RelationToPlayer.RoughlyTheSame);
 			// Randomize the opponent (appearance, name, broom upgrades, equipped spells, minimap icon color)
 			opponent.Randomize(opponentColors[i % opponentColors.Length]);
 		}
