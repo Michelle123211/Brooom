242cbeb5d84ea25d343d9849ab15d48472c43a55
Michelle123211 <M.stolova@seznam.cz>
2023-11-15 15:07:41 +0100

    AI - keeping direction mistake based on curve

--- Brooom/Assets/Scripts/AI/Navigation/BasicNavigationGoalExecutor.cs
+++ Brooom/Assets/Scripts/AI/Navigation/BasicNavigationGoalExecutor.cs
@@ -5,31 +5,28 @@ using UnityEngine;
 public class BasicNavigationGoalExecutor : NavigationGoalExecutor {
 
 	[Header("Skill-based mistakes")]
-	[Tooltip("If the agent should make a mistake of not changing the direction to a new goal, the original direction will be kept for this amount of seconds.")]
-	[SerializeField] float keepDirectionDuration = 0.7f;
+	[Tooltip("The curve describes mapping from dexterity mistake probability to duration of not changing the direction to a new goal (the original direction is kept for this amount of seconds).")]
+	[SerializeField] AnimationCurve keepDirectionDurationCurve;
 
 	private AISkillLevel agentSkillLevel;
 
 	protected override Vector3 DetermineTargetPositionFromGoal(NavigationGoal goal) {
-		// TODO: Take into consideration errors depending on the goal type
-
 		// Determine if any mistakes occur
 		goal.DetermineIfShouldFail(); // If the goal should be failed, its target position is adjusted accordingly
 		if (agentSkillLevel == null)
 			agentSkillLevel = agent.GetComponentInChildren<AISkillLevel>();
-		if (Random.value < agentSkillLevel.GetDexterityMistakeProbability()) { // agent should continue in the same direction (according to the dexterity mistake)
-			 // Set target position far in front of the agent
-			StartCoroutine(SetTargetPositionAfterKeepingDirection(goal));
-			return (agent.transform.position + agent.transform.forward * 1000f);
-		}
-		return goal.TargetPosition;
+		// Make sure to set the correct target position after keeping the same direction for a short while
+		StartCoroutine(SetTargetPositionAfterKeepingDirection(goal));
+		// Return target position far in front of the agent (it will be then corrected 
+		return (agent.transform.position + agent.transform.forward * 1000f);
 	}
 
 	private IEnumerator SetTargetPositionAfterKeepingDirection(NavigationGoal goal) {
 		// Wait for a while
+		float keepDirectionDuration = keepDirectionDurationCurve.Evaluate(agentSkillLevel.GetDexterityMistakeProbability());
 		yield return new WaitForSeconds(keepDirectionDuration);
 		// Set the correct target position
 		if (goal.IsSameAs(currentGoal)) // the goal has not been changed in the meantime
-			steering.SetTargetPosition(DetermineTargetPositionFromGoal(goal));
+			steering.SetTargetPosition(goal.TargetPosition);
 	}
 }
