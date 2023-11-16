c4897e30b529c9fcf3ca0b765c2eba710989b99b
Michelle123211 <M.stolova@seznam.cz>
2023-11-16 03:03:07 +0100

    AI - prepared curves for other mistakes

--- Brooom/Assets/Scripts/AI/AISkillBasedMistakesParameters.cs
+++ Brooom/Assets/Scripts/AI/AISkillBasedMistakesParameters.cs
@@ -11,17 +11,23 @@ public class AISkillBasedMistakesParameters : ScriptableObject {
 	public AnimationCurve speedModifierCurve;
 	[Tooltip("The curve describes mapping from dexterity mistake probability to duration of not changing the direction to a new goal (the original direction is kept for this amount of seconds).")]
 	public AnimationCurve keepDirectionDurationCurve;
-	[Tooltip("The curve describes weight (between 0 and 1) of the direction to avoid collision based on the probability of mistake (average of dexterity and precision mistake probabilities).")]
+	[Tooltip("The curve describes weight (between 0 and 1) of the direction to avoid collision based on the probability of corresponding mistake (average of dexterity and precision mistake probabilities).")]
 	public AnimationCurve collisionAvoidanceWeightCurve;
 
-	//[Header("Hoops and checkpoints")]
-	// TODO: Skip hoop/checkpoint
-	// TODO: Miss hoop
+	[Header("Hoops and checkpoints")]
+	[Tooltip("The curve describes probability of skipping a hoop (checkpoint cannot be skipped) based on the precision mistake probability.")]
+	public AnimationCurve hoopSkipCurve;
+	[Tooltip("The curve describes distance (normalized) from the ideal target position of a hoop/checkpoint based on the dexterity mistake probability.")]
+	public AnimationCurve hoopCheckpointMissCurve;
 
-	//[Header("Bonuses")]
-	// TODO: Skip speed bonus
-	// TODO: Skip mana bonus
-	// TODO: Skip bonus
-	// TODO: Miss bonus
+	[Header("Bonuses")]
+	[Tooltip("The curve describes probability of skipping a speed bonus based on the probability of corresponding mistake (average of speed and precision mistake probabilities).")]
+	public AnimationCurve speedBonusSkipCurve;
+	[Tooltip("The curve describes probability of skipping a mana bonus based on the probability of corresponding mistake (average of precision and magic mistake probabilities).")]
+	public AnimationCurve manaBonusSkipCurve;
+	[Tooltip("The curve describes probability of skipping a bonus (other than speed and mana) based on the precision mistake probability.")]
+	public AnimationCurve bonusSkipCurve;
+	[Tooltip("The curve describes distance (normalized) from the bonus centre which is used as the target position based on the dexterity mistake probability.")]
+	public AnimationCurve bonusMissCurve;
 
 }
