5b772eb21fe24b5b25aa421e30663456e2ac72a8
Michelle123211 <M.stolova@seznam.cz>
2024-10-30 16:33:29 +0100

    Mana regeneration
    - Mana is slowly regenerating over time

---
+++ Brooom/Assets/Scripts/Gameplay/Spells/ManaRegeneration.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManaRegeneration : MonoBehaviour {
+
+	[Tooltip("Spell Controller component managing mana amount. It is used to change the amount.")]
+	[SerializeField] SpellController spellController;
+
+	[Tooltip("After how many seconds the mana is automatically regenerated.")]
+	[SerializeField] float manaRegenerationInterval = 5f;
+	[Tooltip("How much mana is regenerated each time.")]
+	[SerializeField] int manaRegenerationAmount = 5;
+
+	[SerializeField] bool debugLogs;
+
+	private float timeUntilManaRegeneration;
+
+	void Update() {
+		// Mana regeneration
+		timeUntilManaRegeneration -= Time.deltaTime;
+		if (timeUntilManaRegeneration < 0) {
+			spellController.ChangeManaAmount(manaRegenerationAmount);
+			if (debugLogs) Debug.Log($"Current mana increased to {spellController.CurrentMana}/{spellController.MaxMana}.");
+			timeUntilManaRegeneration += manaRegenerationInterval;
+		}
+	}
+
+	private void OnEnable() {
+		timeUntilManaRegeneration = manaRegenerationInterval;
+	}
+
+}
