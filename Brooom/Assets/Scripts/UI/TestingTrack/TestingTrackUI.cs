defa7fdfa1a662ff7350a0745cb0676595f124d2
Michelle123211 <M.stolova@seznam.cz>
2024-10-31 15:17:38 +0100

    Testing Track HUD
    - Add speed and altitude
    - Test all parts

--- Brooom/Assets/Scripts/UI/TestingTrack/TestingTrackUI.cs
+++ Brooom/Assets/Scripts/UI/TestingTrack/TestingTrackUI.cs
@@ -1,23 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+using TMPro;
 
 public class TestingTrackUI : MonoBehaviour {
 
+	[Header("Basic info")]
+	[SerializeField] TextMeshProUGUI speedText;
+	[SerializeField] TextMeshProUGUI altitudeText;
+
+	[Header("Spells")]
 	[SerializeField] RaceSpellsUI spellsUI; // TODO: Delete, only temporary
+
+	private CharacterMovementController playerMovementController;
+
+	public void UpdatePlayerState() {
+		speedText.text = Math.Round(playerMovementController.GetCurrentSpeed(), 1).ToString();
+		altitudeText.text = Math.Round(playerMovementController.GetCurrentAltitude(), 1).ToString();
+	}
 
 	public void ReturnBack() {
         SceneLoader.Instance.LoadScene(Scene.PlayerOverview);
     }
 
 	private void Start() {
-		CharacterRaceState playerRaceState = UtilsMonoBehaviour.FindObjectOfTypeAndTag<CharacterRaceState>("Player");
-		spellsUI.Initialize(playerRaceState.gameObject); // initialize and show
+		playerMovementController = UtilsMonoBehaviour.FindObjectOfTypeAndTag<CharacterMovementController>("Player");
+		spellsUI.Initialize(playerMovementController.gameObject); // initialize and show
 	}
 
 	private void Update() {
 		if (InputManager.Instance.GetBoolValue("Pause"))
 			ReturnBack();
+		UpdatePlayerState();
 	}
 
 }
