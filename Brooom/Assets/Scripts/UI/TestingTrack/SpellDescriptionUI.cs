622eee926071dd85b0b4d6ad1551049c2ed7b6f7
Michelle123211 <M.stolova@seznam.cz>
2024-11-01 10:21:59 +0100

    Testing Track - formatted spell description

--- Brooom/Assets/Scripts/UI/TestingTrack/SpellDescriptionUI.cs
+++ Brooom/Assets/Scripts/UI/TestingTrack/SpellDescriptionUI.cs
@@ -5,6 +5,18 @@ using TMPro;
 
 public class SpellDescriptionUI : MonoBehaviour {
 
+	[Header("Text")]
+	[Tooltip("Default color of the text (when no custom tags are applied).")]
+	[SerializeField] Color textColor = Color.white;
+	[Tooltip("Font used for all the tooltip text.")]
+	[SerializeField] TMP_FontAsset font;
+
+	[Header("Colors")] // TODO: take colors from a color palette
+	[SerializeField] Color spellNameColor;
+	[SerializeField] Color manaColor;
+	[SerializeField] Color cooldownColor;
+
+	[Header("UI elements")]
 	[SerializeField] TextMeshProUGUI spellNameLabel;
 	[SerializeField] TextMeshProUGUI manaCostLabel;
 	[SerializeField] TextMeshProUGUI spellDescriptionLabel;
@@ -33,12 +45,27 @@ public class SpellDescriptionUI : MonoBehaviour {
 
 	private void SetSpellDescriptionContent() {
 		string keyPrefix = $"Spell{spell.Identifier}";
-		spellNameLabel.text = $"{spell.SpellName}";
-		manaCostLabel.text = $"{LocalizationManager.Instance.GetLocalizedString("SpellInfoMana")}: {spell.ManaCost}";
+		// Use TMPro tags to format the text
+		spellNameLabel.text = $"<color={spellNameColor.ToHex()}><b>{spell.SpellName}</b></color>"; // color and bold
+		manaCostLabel.text = $"<color={manaColor.ToHex()}><b>{LocalizationManager.Instance.GetLocalizedString("SpellInfoMana")}: {spell.ManaCost}</b></color>"; // color and bold
 		spellDescriptionLabel.text = LocalizationManager.Instance.GetLocalizedString($"{keyPrefix}Description");
-		targetLabel.text = $"{LocalizationManager.Instance.GetLocalizedString("SpellInfoTarget")}: {LocalizationManager.Instance.GetLocalizedString($"{keyPrefix}Target")}";
-		cooldownLabel.text = $"{LocalizationManager.Instance.GetLocalizedString("SpellInfoCooldown")} {spell.Cooldown} s";
+		targetLabel.text = $"<b>{LocalizationManager.Instance.GetLocalizedString("SpellInfoTarget")}:</b> {LocalizationManager.Instance.GetLocalizedString($"{keyPrefix}Target")}"; // bold
+		cooldownLabel.text = $"<color={cooldownColor.ToHex()}><b>{LocalizationManager.Instance.GetLocalizedString("SpellInfoCooldown")} {spell.Cooldown} s</b></color>"; // color and bold
 		gameObject.TweenAwareEnable();
+	}
+
+	private void Start() {
+		// Set default font and text color
+		spellNameLabel.font = font;
+		spellNameLabel.color = textColor;
+		manaCostLabel.font = font;
+		manaCostLabel.color = textColor;
+		spellDescriptionLabel.font = font;
+		spellDescriptionLabel.color = textColor;
+		targetLabel.font = font;
+		targetLabel.color = textColor;
+		cooldownLabel.font = font;
+		cooldownLabel.color = textColor;
 	}
 
 }
