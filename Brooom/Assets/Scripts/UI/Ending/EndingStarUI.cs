506bc6b16478c13da11ca91fb4fcd262f8249dfd
Michelle123211 <M.stolova@seznam.cz>
2025-02-08 00:42:33 +0100

    Ending screen
    - Sprites for cup and stars
    - Tween for cup to scale up and then rotate a bit from side to side
    - Animation for a star to spawn
    - Spawning stars in random intervals

---
+++ Brooom/Assets/Scripts/UI/Ending/EndingStarUI.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndingStarUI : MonoBehaviour {
+
+	[Tooltip("Whether the star is on the left side or the right side of the screen. That affects a trail animation, from which side it is filled.")]
+	[SerializeField] bool isOnLeft = true;
+
+	[SerializeField] Image starImage;
+	[SerializeField] Image trailImage;
+
+	private Animator animator;
+
+	public void SetColor(Color color) {
+		starImage.color = color.WithA(1);
+		trailImage.color = color.WithA(0.6f);
+	}
+
+	public bool IsIdle() {
+		return this.animator.GetCurrentAnimatorStateInfo(0).IsName("StarIdle");
+	}
+
+	public void PlaySpawnAnimation() {
+		this.animator.SetTrigger("Spawn");
+	}
+
+	private void Awake() {
+		this.animator = GetComponent<Animator>();
+		this.animator.SetBool("ToTheLeft", isOnLeft);
+	}
+
+}
--- Brooom/Assets/Scripts/UI/Ending/EndingUI.cs
+++ Brooom/Assets/Scripts/UI/Ending/EndingUI.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EndingUI : MonoBehaviour {
+
+    EndingStarUI[] stars;
+    bool[] isStarBeingSpawn;
+
+    float timeToNextStar = 1.0f;
+
     public void ContinueRacing() {
         SceneLoader.Instance.LoadScene(Scene.PlayerOverview);
     }
@@ -11,7 +17,35 @@ public class EndingUI : MonoBehaviour {
         SceneLoader.Instance.LoadScene(Scene.MainMenu);
     }
 
-	private void Start() {
-		// TODO: Mark in PlayerState that the game is complete
+	private void Update() {
+        // Spawn a random star after a random interval
+        timeToNextStar -= Time.deltaTime;
+        if (timeToNextStar < 0) {
+            // Choose a new time
+            timeToNextStar += Random.Range(0.1f, 0.3f);
+            // Choose a random star from the ones which are ready
+            int readyStarsCount = 0;
+            for (int i = 0; i < stars.Length; i++) {
+                isStarBeingSpawn[i] = !stars[i].IsIdle();
+                if (!isStarBeingSpawn[i]) readyStarsCount++;
+            }
+            int randomIndex = Random.Range(0, readyStarsCount);
+            for (int i = 0; i < stars.Length; i++) {
+                if (randomIndex == 0) {
+                    stars[i].PlaySpawnAnimation();
+                    break;
+                }
+                if (!isStarBeingSpawn[i]) randomIndex--;
+            }
+        }
 	}
+
+	private void Start() {
+        PlayerState.Instance.GameComplete = true;
+
+        // Get all stars
+        stars = GetComponentsInChildren<EndingStarUI>();
+        isStarBeingSpawn = new bool[stars.Length];
+    }
+
 }
