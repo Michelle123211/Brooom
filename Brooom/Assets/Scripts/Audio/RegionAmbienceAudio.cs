7247f584d3bd8303c2f332cd8ce299302a1665fc
Michelle123211 <M.stolova@seznam.cz>
2025-03-08 02:25:44 +0100

    Audio - ambience in Testing Track
    - Use `FMODStudioParameterTrigger` to change `Region` parameter
    - Create `TestingTrackAmbience` component to change `Altitude` parameter
    - Adjust borders to fit triggers nicely in the level

--- Brooom/Assets/Scripts/Audio/RegionAmbienceAudio.cs
+++ Brooom/Assets/Scripts/Audio/RegionAmbienceAudio.cs
@@ -7,9 +7,30 @@ public class RegionAmbienceAudio : MonoBehaviour {
 	[SerializeField] FMODUnity.StudioEventEmitter ambienceSound;
 	[SerializeField] FMODUnity.StudioEventEmitter underwaterSnapshot;
 
-	private LevelRegionType lastRegion = LevelRegionType.NONE;
+	protected LevelRegionType LastRegion { get; private set; }  = LevelRegionType.NONE;
 
 	private bool underwater = false;
+
+	protected virtual Vector3 GetCurrentPlayerPosition() {
+		return RaceController.Instance.playerRacer.characterController.transform.position;
+	}
+
+	// Returns LevelRegionType.NONE if there is no need for change
+	protected virtual LevelRegionType GetCurrentRegion(Vector3 playerPosition) {
+		TerrainPoint nearestTerrainPoint = RaceController.Instance.level.GetNearestGridPoint(playerPosition);
+		LevelRegionType region = nearestTerrainPoint.region;
+		if (region != LastRegion) {
+			return region;
+		} else {
+			return LevelRegionType.NONE;
+		}
+	}
+
+	protected virtual float GetCurrentAltitude(Vector3 playerPosition) {
+		TerrainPoint nearestTerrainPoint = RaceController.Instance.level.GetNearestGridPoint(playerPosition);
+		float altitudeAboveGround = playerPosition.y - Mathf.Max(0, nearestTerrainPoint.position.y); // above terrain or water level, whichever is higher
+		return altitudeAboveGround; // would not work for more complicated terrain (e.g. tunnel)
+	}
 
 	private float GetFMODRegionParameterValue(LevelRegionType region) {
 		return region switch {
@@ -24,18 +45,17 @@ public class RegionAmbienceAudio : MonoBehaviour {
 	}
 
 	private void Update() {
+		Vector3 playerPosition = GetCurrentPlayerPosition();
+
 		// Set Region parameter - only if it changed
-		Vector3 playerPosition = RaceController.Instance.playerRacer.characterController.transform.position;
-		TerrainPoint nearestTerrainPoint = RaceController.Instance.level.GetNearestGridPoint(playerPosition);
-		LevelRegionType region = nearestTerrainPoint.region;
-		if (region != lastRegion) {
-			lastRegion = region;
+		LevelRegionType region = GetCurrentRegion(playerPosition);
+		if (region != LevelRegionType.NONE) {
+			LastRegion = region;
 			ambienceSound.SetParameter("Region", GetFMODRegionParameterValue(region));
 		}
 
 		// Set Altitude parameter
-		float altitudeAboveGround = playerPosition.y - Mathf.Max(0, nearestTerrainPoint.position.y); // above terrain or water level, whichever is higher
-		ambienceSound.SetParameter("Altitude", altitudeAboveGround); // would not work for more complicated terrain (e.g. tunnel)
+		ambienceSound.SetParameter("Altitude", GetCurrentAltitude(playerPosition));
 
 		// Handle being underwater
 		if (playerPosition.y < 0 && !underwater) { // Play underwater effect
