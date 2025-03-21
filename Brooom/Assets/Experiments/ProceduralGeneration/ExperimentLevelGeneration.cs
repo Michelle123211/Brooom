0343b0530b2fb1cda5104f0f7af78656858d0f63
Michelle123211 <M.stolova@seznam.cz>
2025-03-21 23:40:16 +0100

    Prevent track intersecting itself
    - Rewrite logic for generating track to reliably prevent intersections - accumulate global angle, do not allow more than 90 degrees in any direction

--- Brooom/Assets/Experiments/ProceduralGeneration/ExperimentLevelGeneration.cs
+++ Brooom/Assets/Experiments/ProceduralGeneration/ExperimentLevelGeneration.cs
@@ -9,6 +9,7 @@ public class ExperimentLevelGeneration : MonoBehaviour {
 
     private void OnLevelGenerated(LevelRepresentation level) {
         canGenerateAnotherLevel = true;
+        Debug.Log("Level generated.");
     }
 
     private void GenerateLevel() {
@@ -26,19 +27,26 @@ public class ExperimentLevelGeneration : MonoBehaviour {
         Dictionary<LevelRegionType, bool> regionsAvailability = new Dictionary<LevelRegionType, bool>();
         regionsAvailability[LevelRegionType.AboveWater] = true;
         //regionsAvailability[LevelRegionType.EnchantedForest] = true;
-        //regionsAvailability[LevelRegionType.AridDesert] = true;
-        regionsAvailability[LevelRegionType.SnowyMountain] = true;
+        regionsAvailability[LevelRegionType.AridDesert] = true;
+        //regionsAvailability[LevelRegionType.SnowyMountain] = true;
         //regionsAvailability[LevelRegionType.BloomingMeadow] = true;
         //regionsAvailability[LevelRegionType.StormyArea] = true;
         levelGenerator.regionsAvailability = regionsAvailability;
         levelGenerator.regionsVisited = regionsAvailability; // all available regions have been visited
-        levelGenerator.terrainRegionsToInclude = new List<LevelRegionType> { LevelRegionType.AboveWater, LevelRegionType.SnowyMountain };
-        PlayerState.Instance.maxAltitude = 30;
+        levelGenerator.terrainRegionsToInclude = new List<LevelRegionType> { LevelRegionType.AboveWater, LevelRegionType.AridDesert };
+        //PlayerState.Instance.maxAltitude = 30;
+        // Set module parameters
+        TrackPointsGenerationRandomWalk trackGenerator = levelGenerator.GetComponent<TrackPointsGenerationRandomWalk>();
+        trackGenerator.numberOfCheckpoints = 4; //20;
+        trackGenerator.maxDirectionChangeAngle = new Vector2(10, 20); //new Vector2(30, 45);
+        trackGenerator.distanceRange = new Vector2(40, 50); //new Vector2(80, 100);
+        levelGenerator.GetComponent<TrackObjectsPlacement>().hoopScale = 1f; //0.4f;
+        levelGenerator.GetComponent<MaximumAngleCorrection>().maxAngle = new Vector2(10, 20).x; //new Vector2(30, 45).x;
         // Generate level
         GenerateLevel();
     }
 
-	private void Update() {
+    private void Update() {
         if (Input.GetKeyDown(KeyCode.Space) && canGenerateAnotherLevel)
             GenerateLevel();
 	}
--- Brooom/Assets/Scripts/ProceduralGeneration/Modules/TrackGeneration/TrackPointsGenerationRandomWalk.cs
+++ Brooom/Assets/Scripts/ProceduralGeneration/Modules/TrackGeneration/TrackPointsGenerationRandomWalk.cs
@@ -22,9 +22,12 @@ public class TrackPointsGenerationRandomWalk : LevelGeneratorModule {
 	public float playerStartDistance = 60f;
 
 
+	private float globalAngle = 0; // accumulating overall angle while adding new points, to make sure we never turn for more than 180° and intersection is not possible
+	private float lastAngle = 0; // last geenrated angle around Y axis
+
 	// Minimum and maximum position of the track points (computed while centering the track around world origin, used for changing level dimensions)
-	Vector2 minPosition;
-	Vector2 maxPosition;
+	private Vector2 minPosition;
+	private Vector2 maxPosition;
 
 	LevelRepresentation levelDebug;
 
@@ -44,6 +47,10 @@ public class TrackPointsGenerationRandomWalk : LevelGeneratorModule {
 	}
 
 	private void GenerateTrackPoints(LevelRepresentation level) {
+		// Initialize variables
+		globalAngle = 0;
+		lastAngle = 0;
+
 		// Compute total number of track points - after each checkpoint a specific number of hoops follows (except the last checkpoint)
 		int numberOfPoints = numberOfCheckpoints + numberOfHoopsBetween * (numberOfCheckpoints - 1);
 
@@ -61,35 +68,16 @@ public class TrackPointsGenerationRandomWalk : LevelGeneratorModule {
 
 		// Repeat until all the points are generated
 		while (level.track.Count < numberOfPoints) {
-			// Check that the last generated point is suficiently far from all the other ones (if not, generate a new one instead)
-			if (IsLastPointCloseToAnotherOne(level)) {
-				// Remove last two points
-				level.track.RemoveRange(level.track.Count - 2, 2);
-				// Choose next direction vector and distance
-				SelectParametersForNextStep(level, ref direction, ref distance, ref point);
-			}
-			// Check that the last generated segment is not intersecting with any previous one (if it is, then backtrack a few steps and try again)
-			else if (IsLastSegmentIntersectingWithAnyPrevious(level)) {
-				// Remove last few points
-				int removeFromIndex = Mathf.Max(level.track.Count - backtrackDepth - 1, 1);
-				int countToRemove = Mathf.Min(backtrackDepth, level.track.Count - 1);
-				level.track.RemoveRange(removeFromIndex, countToRemove);
-				// Choose next direction vector and distance
-				SelectParametersForNextStep(level, ref direction, ref distance, ref point);
-			}
-			// Otherwise everything is all right and a new point may be generated
-			else {
-				// Move to the next point (given by the previous point, direction vector and distance)
-				nextPoint = new TrackPoint();
-				nextPoint.position = point.position + direction * distance;
-				point = nextPoint;
-				// Add the point to a list
-				level.track.Add(point);
-				// Mark checkpoints
-				if ((level.track.Count - 1) % (numberOfHoopsBetween + 1) == 0) point.isCheckpoint = true;
-				// Choose next direction vector and distance
-				SelectParametersForNextStep(level, ref direction, ref distance, ref point);
-			}
+			// Move to the next point (given by the previous point, direction vector and distance)
+			nextPoint = new TrackPoint();
+			nextPoint.position = point.position + direction * distance;
+			point = nextPoint;
+			// Add the point to a list
+			level.track.Add(point);
+			// Mark checkpoints
+			if ((level.track.Count - 1) % (numberOfHoopsBetween + 1) == 0) point.isCheckpoint = true;
+			// Choose next direction vector and distance
+			SelectParametersForNextStep(level, ref direction, ref distance, ref point);
 		}
 	}
 
@@ -110,21 +98,7 @@ public class TrackPointsGenerationRandomWalk : LevelGeneratorModule {
 		return direction;
 	}
 
-	private float SelectRotationAngleY() {
-		float angle = 0;
-		float randomValue = Random.value;
-		if (randomValue < 0.4f) { // continue turning to the same side
-			float signedMax = (angle / Mathf.Abs(angle)) * maxDirectionChangeAngle.y; // extreme selected based on side
-			angle = Random.Range(Mathf.Min(signedMax, 0), Mathf.Max(signedMax, 0));
-		} else if (randomValue < 0.8f) { // prefer turning
-			angle = Random.Range(maxDirectionChangeAngle.y / 2, maxDirectionChangeAngle.y);
-			if (Random.value < 0.5f)
-				angle *= -1;
-		} else // completely random
-			angle = Random.Range(-maxDirectionChangeAngle.y, maxDirectionChangeAngle.y);
-		return angle;
-	}
-
+	// Select angle up/down
 	private float SelectRotationAngleX(LevelRepresentation level, TrackPoint lastPoint, Vector2 lastDirection) {
 		float angle;
 		if (lastPoint.position.y < 1) // only up possible
@@ -134,7 +108,7 @@ public class TrackPointsGenerationRandomWalk : LevelGeneratorModule {
 		else {
 			float randomValue = Random.value;
 			if (randomValue < 0.4f) { // continue in the same direction
-				float signedMax = lastDirection.y / Mathf.Abs(lastDirection.y) * -maxDirectionChangeAngle.x; // extreme selected based on up/down
+				float signedMax = Mathf.Sign(lastDirection.y) * -maxDirectionChangeAngle.x; // extreme selected based on up/down
 				angle = Random.Range(Mathf.Min(signedMax, 0), Mathf.Max(signedMax, 0));
 			} else { // completely random
 				angle = Random.Range(-maxDirectionChangeAngle.x, maxDirectionChangeAngle.x);
@@ -143,28 +117,57 @@ public class TrackPointsGenerationRandomWalk : LevelGeneratorModule {
 		return angle;
 	}
 
-	private bool IsLastPointCloseToAnotherOne(LevelRepresentation level) {
-		// Check if the last track point is closer to another one than the minimum distance between two points
-		if (level.track.Count < 3) return false;
-		for (int i = 0; i < level.track.Count - 1; i++) {
-			if (Vector3.Distance(level.track[i].position.WithY(0), level.track[level.track.Count - 1].position.WithY(0)) < distanceRange.x) {
-				//Debug.Log($"Last point {level.track[level.track.Count - 1].position.WithY(0)} is too close to {level.track[i].position.WithY(0)}");
-				return true;
-			}
-		}
-		return false;
+	// Select angle left/right
+	private float SelectRotationAngleY() {
+		float randomValue = Random.value;
+		Vector2 angleInterval = GetMinAndMaxAngleY();
+		if (randomValue < 0.4f) { // continue turning to the same side
+			lastAngle = SelectAngleInTheSameDirection(angleInterval);
+		} else if (randomValue < 0.8f) { // prefer bigger turn but in whichever direction
+			lastAngle = SelectAngleForBiggerTurn(angleInterval);
+		} else // completely random
+			lastAngle = SelectAngleRandomly(angleInterval);
+		// Choose an angle from the given interval, but don't turn too much to prevent track intersecting itself
+		globalAngle += lastAngle;
+		return lastAngle;
 	}
 
-	private bool IsLastSegmentIntersectingWithAnyPrevious(LevelRepresentation level) {
-		if (level.track.Count < 3) return false; // there must be at least two segments
-		int lastSegmentStart = level.track.Count - 2;
-		for (int firstSegmentStart = 0; firstSegmentStart < lastSegmentStart; firstSegmentStart++) {
-			if (Utils.AreSegmentsIntersectingXZ(level.track[firstSegmentStart].position, level.track[firstSegmentStart + 1].position,
-										   level.track[lastSegmentStart].position, level.track[lastSegmentStart + 1].position)) {
-				return true;
-			}
+	private float SelectAngleInTheSameDirection(Vector2 angleInterval) {
+		// Choose random angle to the correct side
+		float direction = Mathf.Sign(lastAngle);
+		if (direction < 0) return Random.Range(angleInterval.x, 0);
+		else return Random.Range(0, angleInterval.y);
+	}
+
+	private float SelectAngleForBiggerTurn(Vector2 angleInterval) {
+		// First check to which side it is possible to turn - if both, choose by random
+		bool isLeftPossible = angleInterval.x < (-maxDirectionChangeAngle.y / 2);
+		bool isRightPossible = angleInterval.y > (maxDirectionChangeAngle.y / 2);
+		int direction = 1;
+		if (isLeftPossible && isRightPossible) direction = (Random.value < 0.5f ? -1 : 1);
+		else if (isLeftPossible) direction = -1;
+		// Then select angle randomly from that direction
+		if (direction < 0) {
+			return Random.Range(angleInterval.x, -maxDirectionChangeAngle.y / 2);
+		} else {
+			return Random.Range(maxDirectionChangeAngle.y / 2, angleInterval.y);
 		}
-		return false;
+	}
+
+	private float SelectAngleRandomly(Vector2 angleInterval) {
+		return Random.Range(angleInterval.x, angleInterval.y);
+		//float angle = Random.Range(-maxDirectionChangeAngle.y, maxDirectionChangeAngle.y);
+	}
+
+	private Vector2 GetMinAndMaxAngleY() {
+		// Select minimum and maximum angle of rotation around Y (left/right) so that no intersections can happen (global angle cannot go over 180 in any direction)
+		Vector2 minMaxAngle = new Vector2(-maxDirectionChangeAngle.y, maxDirectionChangeAngle.y);
+		if (globalAngle < 0) { // there is more space to turn right (positive angle)
+			minMaxAngle.x = Mathf.Max(-90 - globalAngle, -maxDirectionChangeAngle.y);
+		} else { // there is more space to turn left (negative angle)
+			minMaxAngle.y = Mathf.Min(90 - globalAngle, maxDirectionChangeAngle.y);
+		}
+		return minMaxAngle;
 	}
 
 	// Moves all the points so that the final track is centered around the world origin
