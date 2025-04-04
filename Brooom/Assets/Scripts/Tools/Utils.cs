0f693c72c4c508fa8266abf64078cf840f444750
Michelle123211 <M.stolova@seznam.cz>
2025-04-04 14:23:08 +0200

    Forgotten file to commit

--- Brooom/Assets/Scripts/Tools/Utils.cs
+++ Brooom/Assets/Scripts/Tools/Utils.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.Events;
 
 
@@ -112,32 +111,6 @@ public static class Utils
 
     public static Vector3 WithZ(this Vector3 v, float z)
         => new Vector3(v.x, v.y, z);
-
-
-    public static List<T> FindObjects<T>() where T : Component {
-        List<T> result = new List<T>();
-        foreach (GameObject root in SceneManager.GetActiveScene().GetRootGameObjects()) {
-            AddHidden(root, result);
-        }
-        return result;
-    }
-    public static T FindObject<T>() where T : Component {
-        List<T> result = FindObjects<T>();
-        if (result == null || result.Count == 0) return null;
-        else return result[0];
-    }
-
-
-    private static void AddHidden<T>(GameObject root, List<T> result) where T : Component {
-        if (root == null) return;
-        foreach (T component in root.GetComponents<T>()) {
-            result.Add(component);
-        }
-
-        for (int i = 0; i < root.transform.childCount; i++) {
-            AddHidden(root.transform.GetChild(i).gameObject, result);
-        }
-    }
 
 
     public static void TweenAwareEnable(this GameObject go) {
