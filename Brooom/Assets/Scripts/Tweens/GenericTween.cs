6c623a27ece1f879293c64342525842d530d48e1
Michelle123211 <M.stolova@seznam.cz>
2025-04-30 09:17:13 +0200

    Code cleanup - `Scripts/Tweens/`
    + Minor changes - e.g. moving `tweenTarget` from `TweenPropertyValues<TValue>` to `TweenProperty<TValue>`, playing panel open sound instead of click when opening Quick Race settings

--- Brooom/Assets/Scripts/Tweens/GenericTween.cs
+++ Brooom/Assets/Scripts/Tweens/GenericTween.cs
@@ -3,8 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+
+/// <summary>
+/// A component allowing to easily tween different properties of common components with different parameters.
+/// It provides method <c>DoTween()</c> for playing the tween and method <c>UndoTween()</c> for playing the tween in reverse.
+/// </summary>
 public class GenericTween : MonoBehaviour {
+
     [Header("Parameters")]
+    [Tooltip("Duration of the tween in seconds.")]
     public float duration = 0.5f;
     [Tooltip("The tween will start with the given delay (not applied to the reverse).")]
     public float delay = 0f;
@@ -12,7 +19,7 @@ public class GenericTween : MonoBehaviour {
     public bool destroy;
     [Tooltip("If the object should be enabled at the start and disabled when the untween is complete.")]
     public bool enable;
-    [Tooltip("After starting the tween, loop it indefinitely.")]
+    [Tooltip("After starting the tween, loop it indefinitely (also considering whether the tween should be reverted after finishing or not).")]
     public bool loop = false;
     [Tooltip("If the tween is started immediately from the start of the object.")]
     public bool playOnAwake = false;
@@ -28,43 +35,57 @@ public class GenericTween : MonoBehaviour {
     public TweenPropertyRotation rotationTween = new TweenPropertyRotation();
     // scale
     public TweenPropertyScale scaleTween = new TweenPropertyScale();
-    // alpha through CanvasGroup
-    [Tooltip("Affects alpha property of the CanvasGroupt component.")]
+    // alpha through CanvasGroup or SpriteRenderer or Image
+    [Tooltip("Affects alpha property of the CanvasGroup, SpriteRenderer or Image component.")]
     public TweenPropertyAlpha alphaTween = new TweenPropertyAlpha();
     // color through SpriteRenderer or Image or Light2D or TextMeshProUGUI or Text
-    [Tooltip("Affects color of the SpriteRenderer, Image, Light2D, TextMeshProUGUI or Text component.")]
+    [Tooltip("Affects color of the SpriteRenderer, Image, Light2D, Light, TextMeshProUGUI or Text component.")]
     public TweenPropertyColor colorTween = new TweenPropertyColor();
     // intensity through Light2D
     [Tooltip("Affects intensity of the Light2D or Light component.")]
     public TweenPropertyIntensity intensityTween = new TweenPropertyIntensity();
 
     [Header("Callbacks")]
+    [Tooltip("Called when the tween is finished (if the tween should be reverted after, then this is called only after the reversed tween finished).")]
     public UnityEvent onTweenComplete = null;
+    [Tooltip("Called when the untween (i.e. reversed tween) is finished.")]
     public UnityEvent onUntweenComplete = null;
 
     private Vector3 initialPosition;
 
 
     private bool tweenIsRunning = false;
-    private bool reversed = false;
+    private bool reversed = false; // if the tween in running in reverse
 
     private float time = 0;
 
+    /// <summary>
+    /// Plays the tween according to the parameters set in the component.
+    /// </summary>
     public void DoTween() {
         reversed = false;
         if (delay == 0) InitializeTween();
         else Invoke(nameof(InitializeTween), delay);
     }
-
+    /// <summary>
+    /// Plays the tween according to the parameters set in the component in reverse.
+    /// </summary>
     public void UndoTween() {
         reversed = true;
         InitializeTween();
     }
 
+    /// <summary>
+    /// Checks whether the tween is playing right now.
+    /// </summary>
+    /// <returns><c>true</c> if the tween is playing, <c>false</c> otherwise.</returns>
     public bool IsPlaying() {
         return tweenIsRunning;
     }
 
+    /// <summary>
+    /// Sets initial values and makes sure the tween is running.
+    /// </summary>
     private void InitializeTween() {
         // Set initial values
         tweenIsRunning = true;
@@ -74,7 +95,9 @@ public class GenericTween : MonoBehaviour {
             gameObject.SetActive(true);
         TweenProperties(t);
     }
-
+    /// <summary>
+    /// Sets final values.
+    /// </summary>
     private void FinalizeTween() {
         // Set final values
         time = 0;
@@ -84,6 +107,7 @@ public class GenericTween : MonoBehaviour {
         TweenProperties(t);
     }
 
+    // Sets initial values and starts the tween if it should be started in Awake()
     private void Awake() {
         positionTween.SetInitialPosition(transform.position);
         rotationTween.SetInitialRotation(transform.localEulerAngles);
@@ -91,13 +115,15 @@ public class GenericTween : MonoBehaviour {
             DoTween();
     }
 
+    // Updates the tween if it is running
     private void Update() {
         if (tweenIsRunning) {
             time += Time.unscaledDeltaTime;
             if (time >= duration) {
-                // Final values
+                // Finalize the tween
                 tweenIsRunning = false;
                 FinalizeTween();
+                // Check if the tween should keep running (loop, reverse)
                 if (!reversed && revertAfter) {
                     tweenIsRunning = true;
                     reversed = true;
@@ -108,11 +134,13 @@ public class GenericTween : MonoBehaviour {
                     reversed = false;
                     return;
                 }
-                if (reversed && !revertAfter) { // untween complete
+                // Invoke callbacks
+                if (reversed && !revertAfter) { // untween complete (playing in reverse but not because of the settings to revert right after playing forward)
                     if (!Utils.IsNullEvent(onUntweenComplete)) onUntweenComplete.Invoke();
                 }
-                if ((!reversed) || (reversed && revertAfter)) // tween complete
+                if ((!reversed) || (reversed && revertAfter)) // tween complete (either playing forward, or playing in reverse right after playing forward because of the settings)
                     if (!Utils.IsNullEvent(onTweenComplete)) onTweenComplete.Invoke();
+                // Destroy if requested
                 if (destroy) Invoke(nameof(DestroySelf), 0.5f);
             } else {
                 // Perform a single step
@@ -123,6 +151,7 @@ public class GenericTween : MonoBehaviour {
         }
     }
 
+    // Updates all tweened properties based on the current time (normalized)
     private void TweenProperties(float time) {
         TweenProperty(positionTween, time);
         TweenProperty(rotationTween, time);
@@ -132,11 +161,13 @@ public class GenericTween : MonoBehaviour {
         TweenProperty(intensityTween, time);
     }
 
+    // Updates the given property based on the current time (normalized), but only if the property should be tweened
     private void TweenProperty<TValue>(TweenProperty<TValue> tweenProperty, float time) {
         if (tweenProperty.tweenThisProperty)
             tweenProperty.SetTweenedProperty(gameObject, time);
     }
 
+    // Destroys itself (used to destroy the object at the end of the tween if requested)
     private void DestroySelf() {
         Destroy(gameObject);
     }
--- Brooom/Assets/Scripts/Tweens/GenericTweenProperties.cs
+++ Brooom/Assets/Scripts/Tweens/GenericTweenProperties.cs
@@ -5,74 +5,141 @@ using UnityEngine.UI;
 using TMPro;
 using UnityEngine.Rendering.Universal;
 
+
+#region Properties which can be tweened
+
+/// <summary>
+/// A base class for representations of properties which can be tweened in <c>GenericTween</c>.
+/// </summary>
+/// <typeparam name="TValue"></typeparam>
 [System.Serializable]
 public abstract class TweenProperty<TValue> {
+
     [Tooltip("If this particular property should be tweened.")]
     public bool tweenThisProperty;
 
+    [Tooltip("Object whose property is tweened. It has to be set only if it is different than the one with the GenericTween component.")]
+    public GameObject tweenTarget;
+
+    // Generic fields are not serialized so they must be in the derived classes explicitly
+
+    /// <summary>
+    /// Sets the tweened property's value to the value of the tween (used for tweening this property) in the given time.
+    /// </summary>
+    /// <param name="defaultTarget">Object whose property is tweened if <c>tweenTarget</c> is not set for this property.</param>
+    /// <param name="time">Number between 0 and 1 (i.e. normalized time).</param>
     public void SetTweenedProperty(GameObject defaultTarget, float time) {
         TweenPropertyValues<TValue> tweenPropertyValues = GetTweenPropertyValues();
-        SetTweenedPropertyInternal(tweenPropertyValues.tweenTarget != null ? tweenPropertyValues.tweenTarget : defaultTarget, time);
+        SetTweenedPropertyInternal(tweenTarget != null ? tweenTarget : defaultTarget, time);
     }
-    // generic fields are not serialized so they must be in the derived classes explicitly
+
+    /// <summary>
+    /// Gets a representation of a tween (containing start value, end value, interpolation curve(s)) used for tweening this property.
+    /// </summary>
+    /// <returns>Tween values for the property.</returns>
     protected abstract TweenPropertyValues<TValue> GetTweenPropertyValues();
+    /// <summary>
+    /// Sets the tweened property's value to the value of the tween (used for tweening this property) in the given time.
+    /// </summary>
+    /// <param name="target">Object whose property is tweened.</param>
+    /// <param name="time">Number between 0 and 1 (i.e. normalized time).</param>
     protected abstract void SetTweenedPropertyInternal(GameObject target, float time);
+
 }
 
+/// <summary>
+/// A class used from <c>GenericTween</c> to tween <c>position</c> property of <c>Transform</c> component.
+/// </summary>
 [System.Serializable]
 public class TweenPropertyPosition : TweenProperty<Vector3> {
+
+    /// <summary>Tween values (i.e. start value, end value, interpolation curve(s)) used for tweening this property.</summary>
     public TweenPropertyValuesVector3 tweenPropertyValues = new TweenPropertyValuesVector3();
 
     private Vector3 initialPosition;
 
+    /// <summary>
+    /// Sets initial position. Tweened value is then added to this position as a relative offset.
+    /// </summary>
+    /// <param name="position">Initial position.</param>
     public void SetInitialPosition(Vector3 position) {
         initialPosition = position;
     }
 
+    /// <inheritdoc/>
     protected override TweenPropertyValues<Vector3> GetTweenPropertyValues() {
         return tweenPropertyValues;
     }
+    /// <inheritdoc/>
     protected override void SetTweenedPropertyInternal(GameObject target, float time) {
         target.transform.position = initialPosition + tweenPropertyValues.GetTweenValue(time);
     }
 }
 
+/// <summary>
+/// A class used from <c>GenericTween</c> to tween rotation property (<c>localEulerAngles</c>) of <c>Transform</c> component.
+/// </summary>
 [System.Serializable]
 public class TweenPropertyRotation : TweenProperty<Vector3> {
+
+    /// <summary>Tween values (i.e. start value, end value, interpolation curve(s)) used for tweening this property.</summary>
     public TweenPropertyValuesVector3 tweenPropertyValues = new TweenPropertyValuesVector3();
 
     private Vector3 initialRotation;
 
+    /// <summary>
+    /// Sets initial rotation. Tweened value is then added to this rotation as a relative offset.
+    /// </summary>
+    /// <param name="rotation">Initial rotation as Euler angles.</param>
     public void SetInitialRotation(Vector3 rotation) {
         initialRotation = rotation;
     }
 
+    /// <inheritdoc/>
     protected override TweenPropertyValues<Vector3> GetTweenPropertyValues() {
         return tweenPropertyValues;
     }
+    /// <inheritdoc/>
     protected override void SetTweenedPropertyInternal(GameObject target, float time) {
         target.transform.localEulerAngles = initialRotation + tweenPropertyValues.GetTweenValue(time);
     }
 }
 
+/// <summary>
+/// A class used from <c>GenericTween</c> to tween scale property (<c>localScale</c>) of <c>Transform</c> component.
+/// </summary>
 [System.Serializable]
 public class TweenPropertyScale : TweenProperty<Vector3> {
+
+    /// <summary>Tween values (i.e. start value, end value, interpolation curve(s)) used for tweening this property.</summary>
     public TweenPropertyValuesVector3 tweenPropertyValues = new TweenPropertyValuesVector3();
+
+    /// <inheritdoc/>
     protected override TweenPropertyValues<Vector3> GetTweenPropertyValues() {
         return tweenPropertyValues;
     }
+    /// <inheritdoc/>
     protected override void SetTweenedPropertyInternal(GameObject target, float time) {
         target.transform.localScale = tweenPropertyValues.GetTweenValue(time);
     }
 }
 
+/// <summary>
+/// A class used from <c>GenericTween</c> to tween alpha property of <c>CanvasGroup</c>, <c>SpriteRenderer</c>, or <c>Image</c> component.
+/// </summary>
 [System.Serializable]
 public class TweenPropertyAlpha : TweenProperty<float> {
+
+    /// <summary>Tween values (i.e. start value, end value, interpolation curve(s)) used for tweening this property.</summary>
     public TweenPropertyValuesFloat tweenPropertyValues = new TweenPropertyValuesFloat();
+
+    /// <inheritdoc/>
     protected override TweenPropertyValues<float> GetTweenPropertyValues() {
         return tweenPropertyValues;
     }
+    /// <inheritdoc/>
     protected override void SetTweenedPropertyInternal(GameObject target, float time) {
+        // Find a suitable component and set its alpha
         float currentAlpha = tweenPropertyValues.GetTweenValue(time);
         var canvasGroup = target.GetComponent<CanvasGroup>();
         if (canvasGroup != null) {
@@ -93,13 +160,23 @@ public class TweenPropertyAlpha : TweenProperty<float> {
     }
 }
 
+/// <summary>
+/// A class used from <c>GenericTween</c> to tween color property of <c>SpriteRenderer</c>, <c>Image</c>, 
+/// <c>Light2D</c>, <c>Light</c>, <c>TextMeshProUGUI</c>, or <c>Text</c> component.
+/// </summary>
 [System.Serializable]
 public class TweenPropertyColor : TweenProperty<Color> {
+
+    /// <summary>Tween values (i.e. start value, end value, interpolation curve(s)) used for tweening this property.</summary>
     public TweenPropertyValuesColor tweenPropertyValues = new TweenPropertyValuesColor();
+
+    /// <inheritdoc/>
     protected override TweenPropertyValues<Color> GetTweenPropertyValues() {
         return tweenPropertyValues;
     }
+    /// <inheritdoc/>
     protected override void SetTweenedPropertyInternal(GameObject target, float time) {
+        // Find a suitable component and set its color
         Color currentColor = tweenPropertyValues.GetTweenValue(time);
         var spriteRenderer = target.GetComponent<SpriteRenderer>();
         if (spriteRenderer != null) {
@@ -135,14 +212,22 @@ public class TweenPropertyColor : TweenProperty<Color> {
     }
 }
 
+/// <summary>
+/// A class used from <c>GenericTween</c> to tween intensity property of <c>Light2D</c>, or <c>Light</c> component.
+/// </summary>
 [System.Serializable]
 public class TweenPropertyIntensity : TweenProperty<float> {
+
+    /// <summary>Tween values (i.e. start value, end value, interpolation curve(s)) used for tweening this property.</summary>
     public TweenPropertyValuesFloat tweenPropertyValues = new TweenPropertyValuesFloat();
+
+    /// <inheritdoc/>
     protected override TweenPropertyValues<float> GetTweenPropertyValues() {
         return tweenPropertyValues;
     }
-
+    /// <inheritdoc/>
     protected override void SetTweenedPropertyInternal(GameObject target, float time) {
+        // Find a suitable component and set its intensity
         var light2d = target.GetComponent<Light2D>();
         if (light2d != null) {
             light2d.intensity = tweenPropertyValues.GetTweenValue(time);
@@ -157,30 +242,54 @@ public class TweenPropertyIntensity : TweenProperty<float> {
     }
 }
 
+#endregion
 
 
 
+#region Values of the tweened properties
 
+/// <summary>
+/// A base class representing a simple tween based on the type of the tweened value.
+/// Start value and end value are specified here as well as a curve (or multiple curves, one for each component) describing the interpolation.
+/// Derived classes are then specialized on a specific type.
+/// </summary>
+/// <typeparam name="TValue">Type of the tweened value.</typeparam>
 [System.Serializable]
 public abstract class TweenPropertyValues<TValue> {
+
+    [Tooltip("Initial value from which the tween starts.")]
     public TValue startValue;
+    [Tooltip("End value in which the tween ends.")]
     public TValue endValue;
+    [Tooltip("A curve or a set of curves (one for each component) describing the interpolation between the start and the end value over time.")]
     public TweenVectorCurves tweenCurves;
-    [Tooltip("Set only if the target object is different than the one with the GenericTween component.")]
-    public GameObject tweenTarget;
 
     public TweenPropertyValues() {
         tweenCurves = new TweenVectorCurves(NumOfComponents);
     }
 
+    /// <summary>Number of components in the value type.</summary>
     public abstract int NumOfComponents { get; }
+    /// <summary>
+    /// Gets the value of the tween in the given time (by interpolating between the start value and end value).
+    /// </summary>
+    /// <param name="t">Number between 0 and 1 (i.e. normalized time).</param>
+    /// <returns>Tweened value in time <c>t</c>.</returns>
     public abstract TValue GetTweenValue(float t);
 }
 
+/// <summary>
+/// A class representing a simple tween of <c>Vector3</c> value.
+/// Start value and end value are specified here as well as a curve (or multiple curves, one for each component) describing the interpolation.
+/// This class also provides a method for getting the tweened value in a specific time.
+/// </summary>
 [System.Serializable]
 public class TweenPropertyValuesVector3 : TweenPropertyValues<Vector3> {
+
+    /// <inheritdoc/>
     public override int NumOfComponents => 3;
 
+    /// <inheritdoc/>
     public override Vector3 GetTweenValue(float t) {
         if (tweenCurves.tweenComponentWise) {
             float newX = startValue.x + tweenCurves.tweenCurves[0].Evaluate(t) * (endValue.x - startValue.x);
@@ -193,19 +302,34 @@ public class TweenPropertyValuesVector3 : TweenPropertyValues<Vector3> {
     }
 }
 
+/// <summary>
+/// A class representing a simple tween of <c>float</c> value.
+/// Start value and end value are specified here as well as a curve describing the interpolation.
+/// This class also provides a method for getting the tweened value in a specific time.
+/// </summary>
 [System.Serializable]
 public class TweenPropertyValuesFloat : TweenPropertyValues<float> {
+
+    /// <inheritdoc/>
     public override int NumOfComponents => 1;
 
+    /// <inheritdoc/>
     public override float GetTweenValue(float t) {
         return startValue + tweenCurves.tweenCurve.Evaluate(t) * (endValue - startValue);
     }
 }
 
+/// <summary>
+/// A class representing a simple tween of <c>Color</c> value.
+/// Start value and end value are specified here as well as a curve (or multiple curves, one for each component) describing the interpolation.
+/// This class also provides a method for getting the tweened value in a specific time.
+/// </summary>
 [System.Serializable]
 public class TweenPropertyValuesColor : TweenPropertyValues<Color> {
+    /// <inheritdoc/>
     public override int NumOfComponents => 4;
 
+    /// <inheritdoc/>
     public override Color GetTweenValue(float t) {
         if (tweenCurves.tweenComponentWise) {
             float newR = startValue.r + tweenCurves.tweenCurves[0].Evaluate(t) * (endValue.r - startValue.r);
@@ -219,29 +343,40 @@ public class TweenPropertyValuesColor : TweenPropertyValues<Color> {
     }
 }
 
+#endregion
 
 
 
-
+/// <summary>
+/// A class containing curves describing interpolation between start value and end value.
+/// If the value type has multiple components, they may be tweened together (using the same curve), or separately (each having its own curve).
+/// </summary>
 [System.Serializable]
 public class TweenVectorCurves {
     // TODO: If tweenComponentWise == false, show tweenCurve, otherwise tweenCurves
-    [Tooltip("A curve describing change from the start value to the end value in time.")]
-    [ConditionalHide("tweenComponentWise", true)]
+
+    [Tooltip("A curve describing interpolation between the start value and the end value (as a number between 0 and 1) over time (normalized between 0 and 1).")]
+    [ConditionalHide("tweenComponentWise", true)] // hidden if tweenComponentWise is true
     public AnimationCurve tweenCurve = AnimationCurve.Linear(0f, 0f, 1f, 1f);
 
     // TODO: Show only if numOfComponents is > 1
-    [Tooltip("Checked means that each component will be tweened separately according to its own animation curve.")]
-    public bool tweenComponentWise; // true = work with the position as a vector, false = tween each axis separately
+    [Tooltip("Whether each vector component should be tweened separately according to its own animation curve.")]
+    public bool tweenComponentWise; // true = tween each axis separately, false = tween the vector as a whole
 
-    [Tooltip("Curves describing change from the start value to the end value in time component-wise.")]
+    [Tooltip("Curves describing component-wise interpolation between the start value and the end value (as a number between 0 and 1) over time (normalized between 0 and 1).")]
     [ConditionalHide("tweenComponentWise")]
     public AnimationCurve[] tweenCurves;
 
+    /// <summary>
+    /// Creates a new instance of <c>TweenVectorCurves</c> describing a tween of value type with the given number of components 
+    /// according to specified curves (initially linear from 0 to 1).
+    /// </summary>
+    /// <param name="numOfComponents">Number of components in the value type.</param>
     public TweenVectorCurves(int numOfComponents = 3) {
         tweenCurves = new AnimationCurve[numOfComponents];
         for (int i = 0; i < numOfComponents; ++i) {
             tweenCurves[i] = AnimationCurve.Linear(0f, 0f, 1f, 1f);
         }
     }
+
 }
